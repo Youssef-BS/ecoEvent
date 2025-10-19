4ff8db9be8396c8df1b610023721f2f604e2259b
MedEzzedineBenZbiba <mohamedezzedine.benzbiba@esprit.tn>
2025-10-19 20:39:34 +0100

    Complet comments and like features to Post module

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Charitize - Messagerie</title>
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/moment@2.29.4/moment.min.js"></script>
+
 
     <!-- Inclure les assets Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
